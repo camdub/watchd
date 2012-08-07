cfc1357885d1f67772322e5153013e26b1084c7e
camdub <cameron29w@gmail.com>
2012-08-06 23:43:37 -0400

    Updated README

---
+++ README.md
@@ -0,0 +1,3 @@
+### Watchd
+
+A work in progress. Thinking about splitting Ember app and rails app in to separate projects.
--- app/assets/javascripts/controllers/repos_filter_controller.js
+++ app/assets/javascripts/controllers/repos_filter_controller.js
@@ -1,5 +1,6 @@
 Watchd.ReposFilterController = Em.ArrayController.extend({
   selected: null,
+  content: ['', 'cam', 'zack'],
 
   init: function() {
     this.set('content', Ember.A(['cam']));
